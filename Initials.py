677b26cc405815628ea31eb6ed530a75b4c1f6b6
gianavalos <gianfco@icloud.com>
2025-01-25 00:41:16 -0600

    Add files via upload
    Here are my initials printed in python code

---
+++ Initials.py
@@ -0,0 +1,21 @@
+# Assignment: HW1
+# File: Initials.py
+# Student: Gian Avalos
+# UT EID: gfa298
+# Course: CS303E
+#
+# Date: 1/18/2025
+# Description of Program: Initials of my name printed in large letters python code in rows.
+
+print()
+print("    GGGGGGGG        FFFFFFFFFFFF        AAAAAAAA    ")
+print("   GGGGGGGGGG       FFFFFFFFFFFF       AAAAAAAAAA   ")  
+print("  GG                FF                AA        AA  ")
+print("  GG                FF                AA        AA  ")
+print("  GG    GGGGGG      FFFFFFFFFF        AAAAAAAAAAAA  ")
+print("  GG    GGGGGG      FFFFFFFFFF        AAAAAAAAAAAA  ")
+print("  GG        GG      FF                AA        AA  ")
+print("  GG        GG      FF                AA        AA  ")
+print("   GGGGGGGGGG   ..  FF            ..  AA        AA  ")
+print("    GGGGGGGG    ..  FF            ..  AA        AA  ")
+print()
